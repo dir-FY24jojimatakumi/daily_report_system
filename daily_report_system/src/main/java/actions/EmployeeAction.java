07beeab21ffd3e4b9c0fb1378f6f755ca2f909f3
dir-FY24jojimatakumi <takumi.jojima.y033@dirbato.co.jp>
2024-04-17 10:38:34 +0900

    従業員newアクションの作成

--- daily_report_system/src/main/java/actions/EmployeeAction.java
+++ daily_report_system/src/main/java/actions/EmployeeAction.java
@@ -38,30 +38,43 @@ public class EmployeeAction extends ActionBase {
      * @throws ServletException
      * @throws IOException
      */
-    public void index() throws ServletException, IOException {
+public void index() throws ServletException, IOException {
 
-        //指定されたページ数の一覧画面に表示するデータを取得
-        int page = getPage();
-        List<EmployeeView> employees = service.getPerPage(page);
+    //指定されたページ数の一覧画面に表示するデータを取得
+    int page = getPage();
+    List<EmployeeView> employees = service.getPerPage(page);
 
-        //全ての従業員データの件数を取得
-        long employeeCount = service.countAll();
+    //全ての従業員データの件数を取得
+    long employeeCount = service.countAll();
 
-        putRequestScope(AttributeConst.EMPLOYEES, employees); //取得した従業員データ
-        putRequestScope(AttributeConst.EMP_COUNT, employeeCount); //全ての従業員データの件数
-        putRequestScope(AttributeConst.PAGE, page); //ページ数
-        putRequestScope(AttributeConst.MAX_ROW, JpaConst.ROW_PER_PAGE); //1ページに表示するレコードの数
+    putRequestScope(AttributeConst.EMPLOYEES, employees); //取得した従業員データ
+    putRequestScope(AttributeConst.EMP_COUNT, employeeCount); //全ての従業員データの件数
+    putRequestScope(AttributeConst.PAGE, page); //ページ数
+    putRequestScope(AttributeConst.MAX_ROW, JpaConst.ROW_PER_PAGE); //1ページに表示するレコードの数
 
-        //セッションにフラッシュメッセージが設定されている場合はリクエストスコープに移し替え、セッションからは削除する
-        String flush = getSessionScope(AttributeConst.FLUSH);
-        if (flush != null) {
-            putRequestScope(AttributeConst.FLUSH, flush);
-            removeSessionScope(AttributeConst.FLUSH);
-        }
-
-        //一覧画面を表示
-        forward(ForwardConst.FW_EMP_INDEX);
-
+    //セッションにフラッシュメッセージが設定されている場合はリクエストスコープに移し替え、セッションからは削除する
+    String flush = getSessionScope(AttributeConst.FLUSH);
+    if (flush != null) {
+        putRequestScope(AttributeConst.FLUSH, flush);
+        removeSessionScope(AttributeConst.FLUSH);
     }
+
+    //一覧画面を表示
+    forward(ForwardConst.FW_EMP_INDEX);
+
+}
+/**
+ * 新規登録画面を表示する
+ * @throws ServletException
+ * @throws IOException
+ */
+public void entryNew() throws ServletException, IOException {
+
+    putRequestScope(AttributeConst.TOKEN, getTokenId()); //CSRF対策用トークン
+    putRequestScope(AttributeConst.EMPLOYEE, new EmployeeView()); //空の従業員インスタンス
+
+    //新規登録画面を表示
+    forward(ForwardConst.FW_EMP_NEW);
+}
 
 }
